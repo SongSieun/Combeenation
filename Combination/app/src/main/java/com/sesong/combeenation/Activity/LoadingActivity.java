2f1353aed594bf1817bb9706a2886174ede194ca
SieunSong <00sieun0608@naver.com>
2018-09-03 23:50:23 +0900

    start project

---
+++ Combination/app/src/main/java/com/sesong/combeenation/Activity/LoadingActivity.java
@@ -0,0 +1,28 @@
+package com.sesong.combeenation.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sesong.combeenation.R;
+
+public class LoadingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_Loading);
+        setContentView(R.layout.activity_loading);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);  // Intent 선언
+                startActivity(intent);   // Intent 시작
+                finish();
+            }
+        }, 2000);  // 로딩화면 시간
+    }
+}
---
+++ Combination/app/src/main/java/com/sesong/combeenation/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sesong.combeenation.Activity;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sesong.combeenation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+    }
+}
