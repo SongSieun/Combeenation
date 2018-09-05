ba26b3e6b2376cfb88c7983acb44996cc96e55c4
SieunSong <00sieun0608@naver.com>
2018-09-05 23:40:19 +0900

    add

--- Combination/app/src/main/java/com/sesong/combeenation/Activity/MainActivity.java
+++ Combination/app/src/main/java/com/sesong/combeenation/Activity/MainActivity.java
@@ -1,11 +1,20 @@
 package com.sesong.combeenation.Activity;
 
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.sesong.combeenation.Adapter.TabPagerAdapter;
 import com.sesong.combeenation.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
         TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(viewPager);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View layout = inflater.inflate(R.layout.dialogcustom, (ViewGroup)findViewById(R.id.layout_root));
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setView(layout);
+                builder.setPositiveButton("추가", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ArrayList<String> addMenu = new ArrayList<String>();
+                    }
+                });
+            }
+        });
 
     }
 }
